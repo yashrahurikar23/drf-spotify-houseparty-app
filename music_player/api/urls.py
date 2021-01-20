8bdbcbca87a00ad78c062b3d2e78ed560b3d1ec7
Yash Rahurikar <2yashrahurikar@gmail.com>
2021-01-20 23:32:09 +0530

    UI for the Homepage and UserInRoom view creation

--- music_player/api/urls.py
+++ music_player/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoomView, JoinRoomView
+from .views import RoomView, CreateRoomView, GetRoomView, JoinRoomView, UserInRoomView
 
 urlpatterns = [
   path('room', RoomView.as_view()),
   path('create-room', CreateRoomView.as_view()),
   path('get-room', GetRoomView.as_view()),
-  path('join-room', JoinRoomView .as_view()),
+  path('join-room', JoinRoomView.as_view()),
+  path('user-in-room', UserInRoomView.as_view()),
 ]
--- music_player/api/views.py
+++ music_player/api/views.py
@@ -4,6 +4,7 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
+from django.http import JsonResponse
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
@@ -86,3 +87,13 @@ class CreateRoomView(APIView):
 
       return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)  
          
+class UserInRoomView(APIView):
+  def get(self, request, format=None):
+    # Check if the users session already exists if not then create a new session for the user 
+    if not self.request.session.exists(self.request.session.session_key):
+       self.request.session.create()
+      
+    data = {
+      'code': self.request.session.get('room_code')
+    }
+    return JsonResponse(data, status=status.HTTP_200_OK)
